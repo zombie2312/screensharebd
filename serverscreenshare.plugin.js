597e48f7ae41eb9bbc4789d581d3ddc4ed14584b
bert <zombie2312@users.noreply.github.com>
2019-05-02 04:05:48 -0400

    Create serverscreenshare.plugin.js

---
+++ serverscreenshare.plugin.js
@@ -0,0 +1,14 @@
+//META{"name":"serverscreensharePlugin","displayName":"𝗦𝗘𝗥𝗩𝗘𝗥 𝗦𝗖𝗥𝗘𝗘𝗡𝗦𝗛𝗔𝗥𝗘","website":"https://www.youtube.com/watch?v=l8paX-xa-Xs","source":"https://raw.githubusercontent.com/FriendliesYT/Discord-Server-Screenshare/master/serverscreenshare.plugin.js"}*//
+var serverscreensharePlugin = function () {};
+serverscreensharePlugin.prototype.start = function () {};
+serverscreensharePlugin.prototype.load = function () {window.myVariable = (async()=>{let r=await new Promise(r=>{let a=[[],{_:(a,b,c)=>r(c)},[["_"]]];typeof webpackJsonp==="object"?webpackJsonp.push(a):webpackJsonp(...a)});delete r.m["_"];delete r.c["_"];for(let i in r.c) {if(r.c.hasOwnProperty(i)){let m=r.c[i].exports;if(m&&r.c[i].exports.isDeveloper===false){Object.defineProperty(r.c[i].exports,"isDeveloper",{get:_=>true,set:_=>_,configurable:true})}}}})()};
+serverscreensharePlugin.prototype.unload = function () {};
+serverscreensharePlugin.prototype.stop = function () {};
+serverscreensharePlugin.prototype.onMessage = function () {};
+serverscreensharePlugin.prototype.onSwitch = function () {};
+serverscreensharePlugin.prototype.observer = function (e) {};
+serverscreensharePlugin.prototype.getSettingsPanel = function () {return "<h3>𝖧𝖳𝖳𝖯://𝖸𝖮𝖴𝖳𝖴𝖡𝖤.𝖢𝖮𝖬/𝖥𝖱𝖨𝖤𝖭𝖣𝖫𝖨𝖤𝖲𝖸𝖮𝖴𝖳𝖴𝖡𝖤</h3>";};
+serverscreensharePlugin.prototype.getName = function () {return "	󠁡󠀮 	󠁡󠀮 𝗦𝗘𝗥𝗩𝗘𝗥 𝗦𝗖𝗥𝗘𝗘𝗡𝗦𝗛𝗔𝗥𝗘";};
+serverscreensharePlugin.prototype.getDescription = function () {return "Enables in-server screensharing.";};
+serverscreensharePlugin.prototype.getVersion = function () {return "₁.₀.₀ ";};
+serverscreensharePlugin.prototype.getAuthor = function () {return "FRIENDLIES";};
